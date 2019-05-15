e4c4cc5113ff479f9396129b9fa6ec2d47124f82
LauraBOwensKY <LauraBOwens@gmail.com>
2019-05-14 22:35:34 -0400

    Split Name into FirstName and LastName

--- TeamPowered/Person.cs
+++ TeamPowered/Person.cs
@@ -5,8 +5,9 @@ using System.Text;
 namespace TeamPowered
 {
     class Person
-        public string Name { get; set; }
-        public string Address {get; set;}
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
     }
 }
